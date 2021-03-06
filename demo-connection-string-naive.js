7340c82762f689131967c535ffadd4dc6b4d7a29
nbogie <neillbogie@googlemail.com>
2021-03-06 20:23:13 +0000

    improve doc

--- demo-connection-string-naive.js
+++ demo-connection-string-naive.js
@@ -2,10 +2,14 @@ const { Client } = require("pg");
 
 async function doDemo() {
   //Bad practice here!
-  //We'd really read this string from an environment variable - not embed it in our code!
-  // const connectionString = "postgresql://dbuser@localhost:5432/mydb";
+  //Good practice would be to read this string from an environment variable
+  //NOT embed it in our code.
+
+  //However, this is the simplest way to see how a pg client can be configured with a connection string
+
   const connectionString =
     "postgresql://dbuser:secretpassword@database.server.com:5432/mydb";
+
   const client = new Client({
     connectionString,
   });
@@ -14,6 +18,7 @@ async function doDemo() {
 
   const result = await client.query("SELECT NOW()");
   console.log(result.rows);
+
   await client.end();
 }
 
