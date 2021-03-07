9e6f442add4d92528b4ac01888253c6e8d60fd56
nbogie <neillbogie@googlemail.com>
2021-03-07 19:33:04 +0000

    add an upsert demo (insert/update) and supporting table

--- create-tables.sql
+++ create-tables.sql
@@ -1,5 +1,10 @@
-DROP TABLE WORDS;
-DROP TABLE CATEGORIES;
+-- Drop the tables if they exist. Order matters.
+DROP TABLE IF EXISTS WORDS;
+DROP TABLE IF EXISTS CATEGORIES;
+DROP TABLE IF EXISTS HISCORES;
+--
+--
+-- Create the tables
 CREATE TABLE categories (id SERIAL PRIMARY KEY, name text not null);
 CREATE TABLE words (
     id serial primary key,
@@ -7,41 +12,60 @@ CREATE TABLE words (
     word text not null,
     CONSTRAINT fk_category FOREIGN KEY(category_id) REFERENCES categories(id)
 );
+CREATE TABLE HISCORES (
+    id SERIAL PRIMARY KEY,
+    username character varying(255) unique,
+    score integer not null,
+    time timestamp without time zone
+);
+--
+--
 -- Insert some data, too.  Not part of the schema.
+--
+--
+-- Add some quiz categories
 insert into categories (name)
 values ('cities'),
     ('sports'),
     ('continents'),
     ('colours'),
     ('coding language');
+--
+--
+-- Add some words for each quiz category
 insert into words (word, category_id)
 values ('liverpool', 1),
     ('birmingham', 1),
-    ('london', 1);
-insert into words (word, category_id)
-values ('football', 2),
+    ('london', 1),
+    ('football', 2),
     ('swimming', 2),
     ('rugby', 2),
-    ('hockey', 2);
-insert into words (word, category_id)
-values ('africa', 3),
+    ('hockey', 2),
+    ('africa', 3),
     ('asia', 3),
     ('europe', 3),
     ('antartica', 3),
     ('north america', 3),
     ('south america', 3),
-    ('oceania', 3);
-insert into words (word, category_id)
-values ('red', 4),
+    ('oceania', 3),
+    ('red', 4),
     ('blue', 4),
     ('green', 4),
     ('pink', 4),
-    ('purple', 4);
-insert into words (word, category_id)
-values ('typescript', 5),
+    ('purple', 4),
+    ('typescript', 5),
     ('javascript', 5),
     ('haskell', 5),
     ('python', 5),
-    ('ruby', 5),
-    ('scala', 5),
     ('c sharp', 5);
+--
+--
+-- Add some hiscores!
+insert into hiscores (
+        username,
+        score,
+        time
+    )
+values ('matt', 650, now()),
+    ('emmanuel', 550, now()),
+    ('hannah', 700, now());
---
+++ demo-upsert.js
@@ -0,0 +1,52 @@
+//An upsert is either an insert or an update.
+//Context: The hiscore table is intended to hold ONE hiscore for any given username.
+//The first "upsert" to hiscore for any new username will be an insert.
+//Subsequent ones for that same username will be updates.
+//Note that the username column has been defined UNIQUE in the schema.
+
+const { Client } = require("pg");
+
+async function doDemo() {
+  const client = new Client(); //defaults to localhost
+  await client.connect();
+
+  //prep the table to a known empty state
+  await client.query("delete from hiscores");
+  console.log("Deleted all hiscores");
+
+  //first score for user neill - should be an insert
+  await upsertScore(client, "neill", 100);
+  console.log("== After first upsert ==");
+  await showScores(client);
+
+  //second score for user neill - should be an update
+  await upsertScore(client, "neill", 999);
+  console.log("== After second upsert ==");
+  await showScores(client);
+
+  await client.end();
+}
+
+doDemo();
+
+/** Inserts or updates a score row for the given user.
+ * Returns the promise returned by client.query.
+ * This promise will resolve to the results including
+ * the inserted or updated row.
+ */
+function upsertScore(client, username, score) {
+  return client.query(
+    `INSERT INTO hiscores(username, score, time)
+    VALUES($1, $2, now()) 
+    ON CONFLICT (username)
+    DO UPDATE SET score = $2, time=now()
+    RETURNING *`,
+    [username, score]
+  );
+}
+
+async function showScores(client) {
+  const res = await client.query("select * from hiscores");
+  console.log("Entire hiscores table is now: ");
+  console.table(res.rows);
+}
