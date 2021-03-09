7ae2eb138a199ee9cbf14debbb8e06e5236a5020
nbogie <neillbogie@googlemail.com>
2021-03-09 22:38:33 +0000

    add demo using LIKE in param'd query

---
+++ demo-parameterised-select-with-like.js
@@ -0,0 +1,17 @@
+const { Client } = require("pg");
+
+async function doDemo() {
+  const client = new Client();
+  await client.connect();
+
+  const searchTerm = "ing";
+
+  const text = "select * from words where word like $1";
+  const values = [`%${searchTerm}%`];
+
+  const res = await client.query(text, values);
+  console.log(res.rows);
+  await client.end();
+}
+
+doDemo();
